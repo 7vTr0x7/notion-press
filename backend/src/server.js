19342d6c9fb246bf0849367e2833e594f03b618b
7vTr0x7 <7vtrox7@gmail.com>
2025-09-25 18:48:09 +0530

    changed limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,10 @@ import bookRouter from "./routes/book.routes.js";
 
 config();
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
 
-app.use(express.json());
+app.use("/api/books", bookRouter);
 
 const PORT = process.env.PORT;
 
